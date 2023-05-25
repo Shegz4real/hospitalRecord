a6878b3a4221228afcb48ac093c5e2b0320d87be
shegz bedimma <sgonzwak@gmail.com>
2023-05-25 16:59:20 +0100

    routes and models

---
+++ models/adminModel.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const adminSchema = new mongoose.Schema({
+    username:{type: String, requires: true},
+    password:{type: String, required: true},
+    isAdmin:{type: Boolean, default: true}
+})
+
+mosdule.exports = mongoose.model('Admin', adminSchema);
---
+++ models/docModel.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const docSchema = mongoose.Schema({
+    username:{type: String, required: true},
+    password:{type:String, required: true},
+    isAdmin:{type: Boolean, default:false}
+})
+
+module.exports= mongoose.model('Doctors', docSchema);
