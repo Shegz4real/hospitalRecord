2215cc9e18e56c799204a4db8b6b5118d612f5b2
shegz bedimma <sgonzwak@gmail.com>
2023-05-30 10:49:56 +0100

    added user type

--- models/pharmModel.js
+++ models/pharmModel.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const pharmSchema = mongoose.Schema({
     username:{type: String, required: true},
     password:{type:String, required: true},
+    user:{type:String, required:true},
     isAdmin:{type: Boolean, default:false}
 })
 
