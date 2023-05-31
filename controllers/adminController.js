7e7c8faf0c6d7b31ed8430decd238c43a7054025
shegz bedimma <sgonzwak@gmail.com>
2023-05-31 11:06:15 +0100

    admin add_user function

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,8 +4,41 @@
     delete user
     clear patient record 
 */
-const admin = require('../models/adminModel')
+const Admin = require('../models/adminModel');
+const Doc = require('../models/docModel');
+const Lab = require('../models/labModel');
+const Pharm = require('../models/pharmModel');
+const Records = require('../models/recordModel');
+
+
 
 exports.signupUser = async(req, res)=>{
-    const {username, password} = req.body;
+    
+    console.log(req.body.user)
+
+    try{
+        if( req.body.user == "doctor"){
+            const doc = new Doc(req.body);
+            await doc.save();
+            res.redirect('/dashboard')
+    
+        } else if(req.body.user == "pharm"){
+            const pharm = new Pharm(req.body);
+            console.log(pharm);s
+            await pharm.save();
+            res.redirect('/dashboard');
+
+        } else if(req.body.user == "lab"){
+            const lab = new Lab(req.body);
+            await lab.save();
+            res.redirect('/dashboard');
+
+        }else{
+            res.send("invalid user");
+        }
+    
+    }catch(err){
+        console.log(err);
+    }
+   
 }
